0e207e64d77c5f1ffc166f098194f300f2f2df22
jessclarence111 <jessclarencearreza@gmail.com>
2022-04-23 14:49:25 +0800

    Add project files.

---
+++ Quiz2/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Quiz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.item1 = new System.Windows.Forms.Button();
+            this.item2 = new System.Windows.Forms.Button();
+            this.item3 = new System.Windows.Forms.Button();
+            this.computeBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.qtyTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.vatAmTextBox = new System.Windows.Forms.TextBox();
+            this.vat12TextBox = new System.Windows.Forms.TextBox();
+            this.totalAmountTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.dcTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.qtyTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.item3);
+            this.groupBox1.Controls.Add(this.item2);
+            this.groupBox1.Controls.Add(this.item1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(398, 376);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Items";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.dcTextBox);
+            this.groupBox2.Controls.Add(this.priceTextBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(450, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(404, 177);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.totalAmountTextBox);
+            this.groupBox3.Controls.Add(this.vat12TextBox);
+            this.groupBox3.Controls.Add(this.vatAmTextBox);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(450, 218);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(404, 193);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Computation";
+            // 
+            // item1
+            // 
+            this.item1.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item1.Location = new System.Drawing.Point(31, 43);
+            this.item1.Name = "item1";
+            this.item1.Size = new System.Drawing.Size(323, 42);
+            this.item1.TabIndex = 3;
+            this.item1.Text = "Item 1";
+            this.item1.UseVisualStyleBackColor = true;
+            this.item1.Click += new System.EventHandler(this.item1_Click);
+            // 
+            // item2
+            // 
+            this.item2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item2.Location = new System.Drawing.Point(31, 100);
+            this.item2.Name = "item2";
+            this.item2.Size = new System.Drawing.Size(323, 42);
+            this.item2.TabIndex = 4;
+            this.item2.Text = "Item 2";
+            this.item2.UseVisualStyleBackColor = true;
+            this.item2.Click += new System.EventHandler(this.item2_Click);
+            // 
+            // item3
+            // 
+            this.item3.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item3.Location = new System.Drawing.Point(31, 160);
+            this.item3.Name = "item3";
+            this.item3.Size = new System.Drawing.Size(323, 42);
+            this.item3.TabIndex = 5;
+            this.item3.Text = "Item 3";
+            this.item3.UseVisualStyleBackColor = true;
+            this.item3.Click += new System.EventHandler(this.item3_Click);
+            // 
+            // computeBtn
+            // 
+            this.computeBtn.Location = new System.Drawing.Point(247, 449);
+            this.computeBtn.Name = "computeBtn";
+            this.computeBtn.Size = new System.Drawing.Size(183, 54);
+            this.computeBtn.TabIndex = 6;
+            this.computeBtn.Text = "Compute";
+            this.computeBtn.UseVisualStyleBackColor = true;
+            this.computeBtn.Click += new System.EventHandler(this.computeBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(450, 449);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(183, 54);
+            this.clearBtn.TabIndex = 7;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 22);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Qty:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 22);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 22);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Discount:";
+            // 
+            // qtyTextBox
+            // 
+            this.qtyTextBox.Location = new System.Drawing.Point(74, 238);
+            this.qtyTextBox.Name = "qtyTextBox";
+            this.qtyTextBox.Size = new System.Drawing.Size(195, 20);
+            this.qtyTextBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(284, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "/pc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "VATable Amount:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 22);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "VAT - 12%: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(44, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 22);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Total Amount:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // vatAmTextBox
+            // 
+            this.vatAmTextBox.Location = new System.Drawing.Point(210, 53);
+            this.vatAmTextBox.Name = "vatAmTextBox";
+            this.vatAmTextBox.Size = new System.Drawing.Size(133, 20);
+            this.vatAmTextBox.TabIndex = 11;
+            // 
+            // vat12TextBox
+            // 
+            this.vat12TextBox.Location = new System.Drawing.Point(210, 94);
+            this.vat12TextBox.Name = "vat12TextBox";
+            this.vat12TextBox.Size = new System.Drawing.Size(133, 20);
+            this.vat12TextBox.TabIndex = 14;
+            // 
+            // totalAmountTextBox
+            // 
+            this.totalAmountTextBox.Location = new System.Drawing.Point(210, 130);
+            this.totalAmountTextBox.Name = "totalAmountTextBox";
+            this.totalAmountTextBox.Size = new System.Drawing.Size(133, 20);
+            this.totalAmountTextBox.TabIndex = 15;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(108, 45);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.ReadOnly = true;
+            this.priceTextBox.Size = new System.Drawing.Size(223, 20);
+            this.priceTextBox.TabIndex = 16;
+            this.priceTextBox.TextChanged += new System.EventHandler(this.priceTextBox_TextChanged);
+            // 
+            // dcTextBox
+            // 
+            this.dcTextBox.Location = new System.Drawing.Point(108, 104);
+            this.dcTextBox.Name = "dcTextBox";
+            this.dcTextBox.ReadOnly = true;
+            this.dcTextBox.Size = new System.Drawing.Size(103, 20);
+            this.dcTextBox.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(217, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 22);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "%";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.computeBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Items Sales Computation";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox qtyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button item3;
+        private System.Windows.Forms.Button item2;
+        private System.Windows.Forms.Button item1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button computeBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.TextBox dcTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox totalAmountTextBox;
+        private System.Windows.Forms.TextBox vat12TextBox;
+        private System.Windows.Forms.TextBox vatAmTextBox;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Quiz2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* Quiz1
+ * Name: Jess Clarence I. Arreza 
+   Teacher: Jay Al Gallenero */
+
+namespace Quiz2
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void priceTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void item1_Click(object sender, EventArgs e)
+        {
+            priceTextBox.Text = "12.00";
+            dcTextBox.Text = "0.00";
+        }
+
+        private void item2_Click(object sender, EventArgs e)
+        {
+            priceTextBox.Text = "20.25";
+            dcTextBox.Text = "5.00";
+        }
+
+        private void item3_Click(object sender, EventArgs e)
+        {
+            priceTextBox.Text = "100.00";
+            dcTextBox.Text = "7.00";
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            priceTextBox.Clear();
+            dcTextBox.Clear();
+            qtyTextBox.Clear();
+            vatAmTextBox.Clear();
+            vat12TextBox.Clear();
+            totalAmountTextBox.Clear();
+        }
+
+        private void computeBtn_Click(object sender, EventArgs e)
+        {
+           
+            if(priceTextBox.Text == "" || dcTextBox.Text == "")
+            {
+                MessageBox.Show("Please Store Items");
+            }
+            else if (qtyTextBox.Text == "")
+            {
+                MessageBox.Show("Put quantity");
+            }
+            else
+            {
+                float price = float.Parse(priceTextBox.Text),
+                      discount = (float.Parse(dcTextBox.Text) / 100),
+                      quantity = float.Parse(qtyTextBox.Text);
+
+
+
+                float vat_amount_12 = (price + (price * discount) * quantity) * (float)0.05;
+                float vat_amount = (quantity * (price * discount)) - vat_amount_12;
+                float total_amount = (price + (price * discount) * quantity);
+
+                vatAmTextBox.Text = vat_amount.ToString();
+                vat12TextBox.Text = vat_amount_12.ToString();
+                totalAmountTextBox.Text = total_amount.ToString(); ;
+
+            }
+           
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
